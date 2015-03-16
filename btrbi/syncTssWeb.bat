562c372a5318a5a11cafe28bfefcf16784e4492d
waitwind <jinpujun@gmail.com>
2015-03-16 20:18:16 +0800

    乾坤大挪移

--- btrbi/syncTssWeb.bat
+++ btrbi/syncTssWeb.bat
@@ -1,6 +1,6 @@
 
-xcopy /Y /S ..\..\TSS\jinhe-tss\webapp\src\main\webapp  src\main\webapp\
+xcopy /Y /S ..\..\jinhe-tss\webapp\src\main\webapp  src\main\webapp\
 
-xcopy /Y /S ..\..\TSS\jinhe-tss\webapp\src\main\resources\freemarker  src\main\resources\freemarker\
-xcopy /Y /S ..\..\TSS\jinhe-tss\webapp\src\main\resources\META-INF  src\main\resources\META-INF\
-xcopy /Y /S ..\..\TSS\jinhe-tss\webapp\src\main\resources\tss  src\main\resources\tss\
+xcopy /Y /S ..\..\jinhe-tss\webapp\src\main\resources\freemarker  src\main\resources\freemarker\
+xcopy /Y /S ..\..\jinhe-tss\webapp\src\main\resources\META-INF  src\main\resources\META-INF\
+xcopy /Y /S ..\..\jinhe-tss\webapp\src\main\resources\tss  src\main\resources\tss\
