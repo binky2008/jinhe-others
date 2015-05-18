9e4598ee770ccfca29b1dc6a6ad758d3be817999
waitwind <jinpujun@gmail.com>
2015-05-18 10:30:01 +0800

    adjust

--- btrbi/syncTss.bat
+++
@@ -1,6 +0,0 @@
-
-xcopy /Y /S ..\..\jinhe-tss\webapp\src\main\webapp  src\main\webapp\
-
-xcopy /Y /S ..\..\jinhe-tss\webapp\src\main\resources\freemarker  src\main\resources\freemarker\
-xcopy /Y /S ..\..\jinhe-tss\webapp\src\main\resources\META-INF  src\main\resources\META-INF\
-::xcopy /Y /S ..\..\jinhe-tss\webapp\src\main\resources\tss  src\main\resources\tss\
